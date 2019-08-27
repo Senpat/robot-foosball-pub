a31bf10b7b844d7ca3d0f617a36436cdda0232e4
Shawn Zhang <pgz11902@gmail.com>
2019-08-26 23:02:44 -0400

    detectbasic first draft

---
+++ detectbasic.py
@@ -0,0 +1,54 @@
+import PIL
+from PIL import Image
+import urllib.request
+import io
+import sys
+import time
+import random
+D = 3
+W = -1
+H = -1																		#dimensions
+
+thresh = 25
+
+
+if __name__ == "__main__":
+	start_time = time.time()
+	random.seed(200)
+
+	#print(PIL.PILLOW_VERSION)
+
+	K = -1
+	f = io.BytesIO(urllib.request.urlopen('https://cf.geekdo-images.com/imagepage/img/5lvEWGGTqWDFmJq_MaZvVD3sPuM=/fit-in/900x600/filters:no_upscale()/pic260745.jpg?fbclid=IwAR2b1R0F-4ssT42HAak69tE8fqZjOah8BEfMgo70js0GcTTPSJBKBMo3C2s').read())
+	for i in range(1,len(sys.argv)):
+		if(sys.argv[i].isdigit()):
+			K = int(sys.argv[i])
+		elif('https' in sys.argv[i]):
+			f = io.BytesIO(urllib.request.urlopen(sys.argv[i]).read())
+		else:
+			f = sys.argv[i]
+
+	img = Image.open(f)
+	pix = img.load()
+	W = img.size[0]
+	H = img.size[1]
+
+	xtotal = 0
+	xcount = 0
+	ytotal = 0
+	ycount = 0
+
+	for x in range(W):
+		for y in range(H):
+			if(p[x,y][0] < thresh and p[x,y][1] < thresh and p[x,y][2] < thresh):
+				xtotal += x
+				ytotal += y
+				xcount += 1
+				ytotal += 1
+
+
+	answerx = xtotal/xcount
+	answery = ytotal/ycount
+	print(str(answerx) + " " + str(answery))
+
+	
